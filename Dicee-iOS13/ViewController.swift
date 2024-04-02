b99f3f57e6eeebe9d5e775242300f7496940cd00
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-04-02 00:56:12 -0300

    Storing Data Using Varables and Arrays

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,25 +11,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
-        
-        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
-        
-        //diceImageViewOne.alpha = 0.5
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button Got Tapped.")
-
-        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceFour")
         
-        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceFour")
+        
+        diceImageViewOne.image = [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ][leftDiceNumber]
+        
+        diceImageViewTwo.image = [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ][rightDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber +  1
+        rightDiceNumber = rightDiceNumber - 1
+        
+        
     }
     
 }
